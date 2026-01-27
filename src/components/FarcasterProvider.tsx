4c280b3e1874403e29260b81ec1770606d66e343
askar-ef <daffaaskar@gmail.com>
2026-01-27 10:35:26 +0700

    fix: remove unused FrameContext type import

--- src/components/FarcasterProvider.tsx
+++ src/components/FarcasterProvider.tsx
@@ -1,15 +1,14 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import sdk, { type FrameContext } from "@farcaster/frame-sdk";
+import sdk from "@farcaster/frame-sdk";
 
 export default function FarcasterProvider({ children }: { children: React.ReactNode }) {
   const [isSDKLoaded, setIsSDKLoaded] = useState(false);
-  const [context, setContext] = useState<FrameContext>();
 
   useEffect(() => {
     const load = async () => {
-      setContext(await sdk.context);
+      const context = await sdk.context;
       sdk.actions.ready();
     };
     if (sdk && !isSDKLoaded) {
